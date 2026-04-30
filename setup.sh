8d374be960496025e739fad85cada4218b62b422
Tawfiq Songne <songnetaufge@gmail.com>
2026-04-30 16:20:54 +0200

    another fix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -i
 
-echo "creazione dell'ambiente in corso...."
+echo "Creazione dell'ambiente in corso...."
 
 python3 -m venv venv
 
@@ -13,10 +13,10 @@ pip install --upgrade pip
 pip install -r requirement.txt
 
 echo "creazione delle directories in corso...."
-mkdir -p data/raw ouput/figures
+mkdir -p data ouput/figures
 
-echo "download del dataset in corso ..."
-curl -sS -L -o data/raw/wue_csv.csv https://huggingface.co/datasets/masterlion/WaterEfficientDatasetForAfricanCountries/resolve/main/Africa_countries_WUE_data.csv
+echo "Download del dataset in corso ..."
+curl -sS -L -o data/wue_csv.csv https://huggingface.co/datasets/masterlion/WaterEfficientDatasetForAfricanCountries/resolve/main/Africa_countries_WUE_data.csv
 
-echo '\n'
+echo "\n"
 echo "setup completato.."
